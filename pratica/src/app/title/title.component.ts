0f2730a506363be887e68af06666513f192dcfc3
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-02-19 20:41:05 -0300

    componente

---
+++ pratica/src/app/title/title.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.scss']
+})
+export class TitleComponent implements OnInit{
+
+  public title:string = "Bem vindo";
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
