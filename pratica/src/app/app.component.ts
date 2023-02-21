adc47e4b5c29f0ae7f36aa78748ead729a2a888d
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-02-21 11:55:56 -0300

    Ciclo de Vida
    DoCheck,
    AfterContentInit,
    AfterContentChecked,
    AfterViewInit,
    AfterViewChecked

--- pratica/src/app/app.component.ts
+++ pratica/src/app/app.component.ts
@@ -1,14 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-
+import {
+   Component,
+   OnInit,
+   DoCheck,
+   AfterContentInit,
+   AfterContentChecked,
+   AfterViewInit,
+   AfterViewChecked
+  } from '@angular/core';
+/*
+DoCheck,
+AfterContentInit,
+AfterContentChecked,
+AfterViewInit,
+AfterViewChecked,
+*/
 @Component({
   selector: 'app-root',
   template: `
-   <app-title></app-title>
+   {{ valor }}
+   <button (click)="adicionar()">Adicionar</button>
    <router-outlet></router-outlet>
   `
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements
+ OnInit,
+ DoCheck,
+ AfterContentInit,
+ AfterContentChecked,
+ AfterViewInit,
+ AfterViewChecked {
+
+  public valor: number = 1;
+
   constructor() { }
 
+  public adicionar(): number{
+    return this.valor += 1;
+  }
+
   ngOnInit(): void { }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck")
+   }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentIni")
+
+   }
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked")
+
+  }
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit")
+
+   }
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked")
+
+   }
 }
+
