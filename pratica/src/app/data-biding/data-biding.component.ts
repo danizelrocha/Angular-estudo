63784f52e9f817a365b2062ee0e2eee4a6e82dd1
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-02-22 11:47:11 -0300

    Pronpety Binding
    Formas de uso

--- pratica/src/app/data-biding/data-biding.component.ts
+++ pratica/src/app/data-biding/data-biding.component.ts
@@ -11,6 +11,11 @@ export class DataBidingComponent implements OnInit {
   public idade: number = 45;
   public maisUm: number = 1;
 
+  public checkedDisabled: boolean = false;
+  public imgSrc:string = /* "https://vidafullstack.com.br/wp-content/uploads/2021/01/como-criar-site-400x250.jpg" */
+  "https://conteudo.imguol.com.br/c/noticias/1c/2022/05/24/imagem-criada-no-imagen-prototipo-do-google-que-cria-imagens-baseadas-em-texto-neste-caso-um-cachorro-corgi-andando-de-bicicleta-na-times-square-usando-oculos-de-sol-e-chapeu-de-praia-1653397634334_v2_900x506.jpg";
+
+  public imgTitle: string = "Property Binding"
   constructor() { }
 
   ngOnInit(): void {
